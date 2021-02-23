2b56b9301a899449f6e469a32b85a1cdb210afd2
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2021-02-23 10:58:37 -0500

    Update Ben.py

--- Ben.py
+++ Ben.py
@@ -2,6 +2,7 @@
 import pyAesCrypt
 import string
 import random
+import os
 
 #Generate Symmetric Key & Store in Variable
 passwd=''
@@ -20,3 +21,10 @@ print("Symmetric Key is: " + sym)
 bufferSize = 64 * 1024
 password = sym
 pyAesCrypt.encryptFile("data.txt", "data.txt.aes", password, bufferSize)
+
+#Delete Original
+os.remove("data.txt")
+
+#Decrypt File
+#pyAesCrypt.decryptFile("data.txt.aes", "dataout.txt", #password, bufferSize)
+#os.remove("data.txt.aes")
